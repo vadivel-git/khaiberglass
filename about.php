5c62a2fa2db43c67b62c5046fe360cd1c10d1381
vadivel-git <srinanbaninfo@gmail.com>
2022-11-08 14:34:33 +0400

    git push

--- about.php
+++ about.php
@@ -41,7 +41,8 @@
                         <div class="pe-lg-1-9">
                             <div class="section-title mb-1-9">
                                 <span class="sm-title">About Us</span>
-                                <h2 class="mb-0 h1">We’re Delivering The Best Customer Experience</h2>
+                                <p>“Khaiber Star Safety Glass Factory L.L.C” was established in 2005 and offers an extensive gamut of services to esteemed clients across a spectrum of industries.</p>
+                                <p>The company was constituted under the management of KSSGF. The company has its own manufacturing facility with a floor area 50,000 sqft. And is able to accommodate the office, store and machinery.</p>
                             </div>
                             <div class="mb-1-9">
                                 <div class="d-flex about-text mb-1-9">
@@ -49,17 +50,8 @@
                                         <i class="fa-brands fa-gitter"></i>
                                     </div>
                                     <div class="flex-grow-1 ms-3">
-                                        <h4 class="h5">Our Mission</h4>
-                                        <p class="mb-0">Our Mission is to be the industry's top-ratede provider issuer enterprise targeting satisfying the most to our clients.</p>
-                                    </div>
-                                </div>
-                                <div class="d-flex about-text">
-                                    <div class="flex-shrink-0">
-                                        <i class="fa-brands fa-unity"></i>
-                                    </div>
-                                    <div class="flex-grow-1 ms-3">
-                                        <h4 class="h5">Our Vision</h4>
-                                        <p class="mb-0">Our Vision is to be a top Web Design company in the IT sector and progress in our current position in the market.</p>
+                                        <h4 class="h5">Mission Statement</h4>
+                                        <p class="mb-0">Provide the highest value to our customers by offering outstanding service, quality products and competitive pricing. Provide regular training and shared information to all employees. Empower each person within the Company framework. Provide opportunities and incentives based on performance to our customers and to our Company.</p>
                                     </div>
                                 </div>
                             </div>
@@ -73,9 +65,44 @@
                             </div>
                         </div>
                     </div>
+
+                    <div class="mb-1-9">
+                        <div class="d-flex about-text mb-1-9">
+
+                            <div class="flex-grow-1 ms-3">
+                                <p class="mb-0">The company is one of the most reliable glass processing company in UAE and has completed projects throughout the country with the help of our own highly qualified engineers and skilled labour. We are able to handle any type of glass works to the satisfaction of our clients.</p>
+                                <p class="mb-0">From the stained glass windows of medieval churches to the renaissance monopoly of mirror makers, flat glass has bought protection to us from the environment while also reflecting its beauty. Today’s flat has become a long way to achieve specific performance, characteristics in terms of strength, visible light transmission, solar reflectance and shading coefficient, thus making it possible to have wide</p>
+                                <br>
+                                <br>
+                                <h4>Statement of Ethics</h4>
+                                <p>At Khaiber Star Safety Glass, ethical conduct and legal compliance are the foundation for our position as an industry leader in the glass and glazing industry. Khaiber Star Safety Glass Factory ability to maintain its leadership position in the industry requires each employee to maintain the highest level of integrity when interacting with vendors, customers and each other.</p>
+                                <p>Khaiber Star Safety Glass Factory maintains certain policies to guide its employees with respect to standards of conduct expected in all areas where improper activities could damage the Company’s reputation and otherwise result in serious adverse consequences to the company and its employees.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <!-- <img src="img/bg/bg-06.png" class="position-absolute left bottom-5 opacity4" alt="..."> -->
+        </section>
+
+        <section class="pt-1-6 pt-md-9">
+            <div class="position-relative">
+                <div class="background-no-repeat position-absolute h-100 w-100 bg-img d-none d-lg-block" data-background="img/content/why-choose-01.jpg">
+                </div>
+                <div class="row g-0 justify-content-end">
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="bg-secondary pt-8 pt-lg-14 pt-xxl-18 pb-8 pb-xxl-13 px-2-2 px-xxl-11 position-relative z-index-9 why-chooseus">
+                            <div class="me-xxl-15">
+                                <p class="text-white mb-2-3 opacity9">Discrimination, harassment, unfair business practices and proper use of the company assets are just a few of the areas in which ethical conduct is a must. It is the responsibility of each employee to foster an environment of integrity, honesty, respect & trust.</p>
+                                <p class="text-white mb-2-3 opacity9">It is with the efforts of all the employees in these areas that Khaiber Star Safety Glass Factory will stay ahead of the industry Standards.</p>
+                                <div class="position-absolute bottom-0 right z-index-1">
+                                    <img src="img/bg/bg-07.png" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
         <!-- FOOTER ================================================== -->
         <?php include"vip/footer.php"; ?>
